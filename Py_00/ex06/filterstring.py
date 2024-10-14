7cc92feb83b755c9c448c3101c6002aaba962c5a
unknown <tristan0922@hotmail.fr>
2024-10-14 12:53:01 +0200

    push

--- Py_00/ex06/filterstring.py
+++ Py_00/ex06/filterstring.py
@@ -1,26 +1,27 @@
 import sys
+from ft_filter import ft_filter
 
-def check_words(number):
-    if number % 2 == 0:
-    #if len(str) > int(sys.argv[2]):
-        return True
-    return False
 
 def main():
     try:
         if len(sys.argv) != 3:
             raise AssertionError
+        #aquarium_tanks = [11, False, 18, 21, "", 12, 34, 0, [], {}]
         iterable = sys.argv[1].split(" ")
         N = int(sys.argv[2])
-        test = ft_filter(check_words, iterable)
-        official = filter(check_words, iterable)
+        test = ft_filter(lambda word: len(word) > N, iterable)
+        official = filter(lambda word: len(word) > N, iterable)
         print(test)
         print(official)
-    except ValueError as e:
+        print(list(test))
+        print(list(official))
+    except ValueError:
         print("AssertionError: the arguments are bad")
     except AssertionError:
         print("AssertionError: the arguments are bad 2")
 
 
 if __name__ == "__main__":
+    #print(filter.__doc__)
+    #print(ft_filter.__doc__)
     main()
--- Py_00/ex06/ft_filter.py
+++ Py_00/ex06/ft_filter.py
@@ -1,9 +1,15 @@
-def ft_filter(function, iterable) -> filter object
-"""
-filter(function or None, iterable) --> filter object
+def ft_filter(function, iterable) -> filter:
+    """
+ft_filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
-"""
-for i in iterable:
-    
+    """
+    if function:
+        for item in iterable:
+            if function(item):
+                yield (item)
+    else:
+        for item in iterable:
+            if item:
+                yield(item)
