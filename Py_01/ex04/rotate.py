880b9a064db1fc4f2d6bda14f23ff0da30bece22
Tristan CHANTROUX <tchantro@made-f0Br7s15.clusters.42paris.fr>
2024-11-05 16:43:14 +0100

    Docstrings

--- Py_01/ex04/rotate.py
+++ Py_01/ex04/rotate.py
@@ -3,6 +3,14 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def rotate(array, size) -> np.array:
+    t_array = np.empty([400, 400], dtype=int)
+    for y in range(size):
+        for x in range(size):
+            t_array[y][x] = array[x][y]
+    return t_array
+
 def zoom(img, size, zoom) -> np.array:
     img = Image.open(img).convert('L')
     w, h = img.size
@@ -13,16 +21,16 @@ def zoom(img, size, zoom) -> np.array:
     bottom = top + size
     img = img.crop((left, top, right, bottom))
     array = np.array(img)
-    print("New shape after slicing:", np.shape(np.expand_dims(array, axis=-1)), "or", np.shape(array))
-    plt.imshow(array, cmap='grey')
+    t_array = rotate(array, size)
+    print("New shape after Transpose:", np.shape(array))
+    plt.imshow(t_array, cmap='grey')
     plt.show()
-    return np.expand_dims(array, axis=-1)
+    return t_array
 
 def main():
-    img = "animal.jpeg"
+    img = 'animal.jpeg'
     print(load_image(img))
     print(zoom(img, 400, 2))
-
 
 if __name__ == "__main__":
     main()
