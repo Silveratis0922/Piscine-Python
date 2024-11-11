093127db200f04340979a04bd8846b765d491d09
unknown <tristan0922@hotmail.fr>
2024-11-11 22:12:38 +0100

    Manage error need for py_01

--- Py_02/ex01/load_csv.py
+++ Py_02/ex01/load_csv.py
@@ -2,17 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# def load(path: str) -> pd.DataFrame:
-#     country = "Andorra"
-#     df = pd.read_csv(path, index_col="country").T
-#     print("Loading dataset of dimensions", df.shape)
-#     plt.plot(df.index, df[country])
-#     plt.title(f"{country} Life expectancy Projections")
-#     plt.xlabel("Year")
-#     plt.xticks(df.index[::40])
-#     plt.ylabel("Life expectancy")
-#     plt.show()
-#     return df
+def load(path: str) -> pd.DataFrame:
+    country = "Andorra"
+    df = pd.read_csv(path, index_col="country").T
+    print("Loading dataset of dimensions", df.shape)
+    plt.plot(df.index, df[country])
+    plt.title(f"{country} Life expectancy Projections")
+    plt.xlabel("Year")
+    plt.xticks(df.index[::40])
+    plt.ylabel("Life expectancy")
+    plt.show()
+    return df
 
 
 # def load(path: str) -> pd.DataFrame: # Series Methode
@@ -29,16 +29,16 @@ import matplotlib.pyplot as plt
 #     plt.show()
 #     return df
 
-def load(path: str) -> pd.DataFrame:
-    country = "France"
-    df = pd.read_csv(path)
-    df = df.loc[df["country"] == country]
-    print(df)
-    print("Loading dataset of dimensions", df.shape)
-    plt.plot(df.columns[1:], df.iloc[0, 1:].values)
-    plt.title(f"{country} Life expenctancy Projections")
-    plt.xlabel("Year")
-    plt.xticks(df.columns[1::40])
-    plt.ylabel("Life expectancy")
-    plt.show()
-    return df
+# def load(path: str) -> pd.DataFrame:
+#     country = "France"
+#     df = pd.read_csv(path)
+#     df = df.loc[df["country"] == country]
+#     print(df)
+#     print("Loading dataset of dimensions", df.shape)
+#     plt.plot(df.columns[1:], df.iloc[0, 1:].values)
+#     plt.title(f"{country} Life expenctancy Projections")
+#     plt.xlabel("Year")
+#     plt.xticks(df.columns[1::40])
+#     plt.ylabel("Life expectancy")
+#     plt.show()
+#     return df
