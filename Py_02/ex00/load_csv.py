5ce99ebdd9a86ff6bb6a2720ad768541b0aedcac
Tristan CHANTROUX <tchantro@paul-f4Br4s5.clusters.42paris.fr>
2024-11-13 16:02:45 +0100

    go home

--- Py_02/ex00/load_csv.py
+++ Py_02/ex00/load_csv.py
@@ -2,6 +2,18 @@ import pandas as pd
 
 
 def load(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path)
-    print("Loading dataset of dimensions", df.shape)
-    return df
+    """
+    Loads a CSV file and returns its DataFrame.
+    """
+    try:
+        if not path.endswith('.csv'):
+            raise AssertionError("Need CSV file.")
+        df = pd.read_csv(path)
+        if df.empty:
+            raise AssertionError("File is empty.")
+        print("Loading dataset of dimensions", df.shape)
+        return df
+    except AssertionError as e:
+        print(f"{AssertionError.__name__}: {e}")
+    except FileNotFoundError:
+        print(f"{FileNotFoundError.__name__}: File does not exist.")
